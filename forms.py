c1c3b57f9dd2689c00d8494c3e21b66c5163bd54
Daniel Roney <bruceroney2014@gmail.com>
2020-09-25 14:23:02 -0400

    Update forms.py

--- forms.py
+++ forms.py
@@ -6,6 +6,6 @@ from wtforms.fields.html5 import DateTimeLocalField
 class CreateEntryForm(FlaskForm):
     title = StringField('title', validators=[DataRequired()])
     timeSpent = IntegerField('timeSpent', validators=[DataRequired(message="must enter a number for this field")])
-    whatILearned = TextAreaField('whatILearned', validators=[DataRequired()])
-    ResourcesToRemember = TextAreaField('ResourcesToRemember', validators=[DataRequired()])
+    learned = TextAreaField('learned', validators=[DataRequired()])
+    resources = TextAreaField('resources', validators=[DataRequired()])
     date = DateTimeLocalField('date', validators=[DataRequired()], format='%Y-%m-%d')
